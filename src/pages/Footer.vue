8e717b27a98687ea8062f2a74f2d6e1936e108f6
Bryan Lucas <bryanluka15@gmail.com>
2023-05-01 21:29:04 -0300

    Added footer in project, added to the github icon the developer profile

---
+++ src/pages/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+<footer>
+    <a>© Copyright, Andromeda Software</a>
+    <a href="https://github.com/Bljexe"><img src="../assets/github.png" alt="Github Icon" id="github-social"></a>
+</footer>
+</template>
+
+<script>
+    export default{
+        name: 'Footer'
+    }
+</script>
+
+<style scoped>
+
+footer{
+    color: var(--color-text-light);
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+}
+
+#github-social{
+    width: 25px;
+}
+
+</style>
